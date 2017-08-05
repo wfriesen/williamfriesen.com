3637649954fc081e948c1ac649222771171cfae9
William Friesen <wfriesen@gmail.com>
2017-08-05 17:34:21 +1000

    add dev config override for local url

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -p 4000:4000 -v `pwd`:/site madduci/docker-github-pages serve --watch --force_polling --host 0.0.0.0
+docker run --rm -it -p 4000:4000 -v `pwd`:/site madduci/docker-github-pages serve --config "./_config.yml,./_config.dev.yml" --watch --force_polling --host 0.0.0.0

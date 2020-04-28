9ad9bca7fc4ce919ad35d1b581bad2ed15baec5b
William Friesen <wfriesen@gmail.com>
2020-04-28 22:56:41 +1000

    switch docker image now that jekyll has moved on

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -p 4000:4000 -v `pwd`:/site madduci/docker-github-pages serve --config "./_config.yml,./_config.dev.yml" --watch --force_polling --host 0.0.0.0
+docker run -it --rm -v $(pwd):/usr/src/app -p "4000:4000" starefossen/github-pages
